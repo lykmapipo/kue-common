385767004369709e894cc0cae16f20ea31aab9d8
lykmapipo <lallyelias87@gmail.com>
2019-05-28 13:42:33 +0300

    chore(example): add http server on sample

--- example/worker.js
+++ example/worker.js
@@ -1,23 +1,40 @@
 'use strict';
 
 /* dependencies */
-const { start, onError, onJobComplete, onJobFailed, dispatch } = require('../');
+const {
+  listen,
+  start,
+  onError,
+  onJobComplete,
+  onJobFailed,
+  dispatch
+} = require('../');
 
+// start queue
 start();
 
-onError((error) => {
+// listen for queue error events
+onError(error => {
   console.log('queue error', error);
 });
 
+// listen for job complete events
 onJobComplete((id, result) => {
   console.log('job id', id);
   console.log('job results', result);
 });
 
-onJobFailed((error) => {
+// listen for job failed events
+onJobFailed(error => {
   console.log('job error', error);
 });
 
+// start http server
+listen(error => {
+  console.log('http error:', error);
+});
+
+// dispatch works
 setInterval(() => {
   dispatch({ type: 'email', data: { to: 'l@j.z' } });
 }, 2000)
